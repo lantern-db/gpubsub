02f684c2c8c8b37abbe6f8c23385bdfce6815bd3
piroyoung <piroyoung@users.noreply.github.com>
2022-04-10 21:11:37 +0900

    add: getter for lastViewedAt

--- gpubsub/message.go
+++ gpubsub/message.go
@@ -18,6 +18,10 @@ func (m *Message[T]) Body() T {
 	return m.body
 }
 
+func (m *Message[T]) LastViewedAt() time.Time {
+	return m.lastViewedAt
+}
+
 func (m *Message[T]) CreatedAt() time.Time {
 	return m.createdAt
 }
